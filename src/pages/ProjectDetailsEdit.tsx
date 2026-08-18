e71144e2ec00b83c027b912de326ac6972b0dfbd
vaishnavim70 <vaishnavimugale70@gmail.com>
2026-08-18 15:37:27 +0530

    project related changes

--- src/pages/ProjectDetailsEdit.tsx
+++ src/pages/ProjectDetailsEdit.tsx
@@ -2035,7 +2035,6 @@ const ProjectDetailsEdit = () => {
           );
           return;
         }
-        if (!validateFile(file, MAX_SIZES[name])) return;
         validFiles.push(file);
       });
 
@@ -5694,7 +5693,7 @@ const ProjectDetailsEdit = () => {
                       <Info className="w-5 h-5 fill-black text-white" />
                       {showTooltipBrochure && (
                         <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1 text-xs text-white bg-gray-900 rounded whitespace-nowrap z-10">
-                          Max Upload Size 5 MB
+                          No file size limit
                         </span>
                       )}
                     </span>
