e1560f63c4f7aa2dba82bc5c59d42de177017464
Atharv Karnekar <karnekaratharv12@gmail.com>
2026-08-25 17:25:45 +0530

    feat: update isHiSocietyUiSite check to include web.lockated.com

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -261,7 +261,7 @@ const isPulseSite = hostname === "pulse.lockated.com";
 const isPanchshilUatSite = hostname === "pulse-uat.panchshil.com";
 const isClubSite = hostname.includes("club.lockated.com");
 const isLocalhost = hostname.includes("localhost") || hostname.includes("127.0.0.1");
-const isHiSocietyUiSite = hostname.includes("ui-hisociety.lockated.com")
+const isHiSocietyUiSite = hostname.includes("ui-hisociety.lockated.com") || hostname === "web.lockated.com";
 const isHiSocietySite =
   isLocalhost ||
   hostname.includes("web.hisociety.lockated.com");
